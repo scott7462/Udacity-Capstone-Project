8e56faf58f862f58f5473ba025d0f6f02efd8e90
Pedro Scott <pedroscott@Pedros-MacBook-Pro.local>
2016-08-19 18:37:46 -0500

    [fead] add the launch and the init activity to manager the dex and the login.

---
+++ Workhard/app/src/main/java/scott/com/workhard/app/ui/init/launch/LaunchActivity.java
@@ -0,0 +1,38 @@
+package scott.com.workhard.app.ui.init.launch;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import scott.com.workhard.R;
+import scott.com.workhard.app.ui.init.InitActivity;
+
+/**
+ * @author pedroscott. scott7462@gmail.com
+ * @version 7/12/16.
+ *          <p>
+ *          Copyright (C) 2015 The Android Open Source Project
+ *          <p/>
+ *          Licensed under the Apache License, Version 2.0 (the "License");
+ *          you may not use this file except in compliance with the License.
+ *          You may obtain a copy of the License at
+ *          <p/>
+ * @see <a href = "http://www.aprenderaprogramar.com" /> http://www.apache.org/licenses/LICENSE-2.0 </a>
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+        InitActivity.newInstance(this);
+        finish();
+    }
+
+}
