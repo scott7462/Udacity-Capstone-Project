6c10221f6875214b2160439fd0732763bf22fdd5
Pedro Scott <pedroscott@Pedros-MacBook-Pro.local>
2016-08-19 18:36:04 -0500

    [fead] add the example of a instrumental test.

---
+++ Workhard/app/src/androidTest/java/scott/com/workhard/app/ui/init/launch/LaunchActivityTest.java
@@ -0,0 +1,42 @@
+package scott.com.workhard.app.ui.init.launch;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import scott.com.workhard.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class LaunchActivityTest {
+
+    @Rule
+    public ActivityTestRule<LaunchActivity> mActivityTestRule = new ActivityTestRule<>(LaunchActivity.class);
+
+    @Test
+    public void launchActivityTest2() {
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.bTFrgLoginButton), withText("Login"), isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction floatingActionButton = onView(
+                allOf(withId(R.id.fab), isDisplayed()));
+        floatingActionButton.perform(click());
+
+    }
+
+}
