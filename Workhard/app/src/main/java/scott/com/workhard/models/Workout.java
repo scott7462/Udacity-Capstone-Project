856085936e367675b88edc916c40b739aecb6af4
Pedro Scott <pedroscott@Pedros-MacBook-Pro.local>
2016-09-04 22:16:59 -0500

    [feat] add new models and tables test elements to databases.

---
+++ Workhard/app/src/main/java/scott/com/workhard/models/Workout.java
@@ -0,0 +1,87 @@
+package scott.com.workhard.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author pedroscott. scott7462@gmail.com
+ * @version 9/4/16.
+ *          <p>
+ *          Copyright (C) 2015 The Android Open Source Project
+ *          <p/>
+ *          Licensed under the Apache License, Version 2.0 (the "License");
+ *          you may not use this file except in compliance with the License.
+ *          You may obtain a copy of the License at
+ *          <p/>
+ * @see <a href = "http://www.aprenderaprogramar.com" /> http://www.apache.org/licenses/LICENSE-2.0 </a>
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+public class Workout {
+
+    @SerializedName("id")
+    @Expose
+    private int id;
+    @SerializedName("rest_between_exercise")
+    @Expose
+    private int restBetweenExercise;
+    @SerializedName("rest_rounds_exercise")
+    @Expose
+    private int restRoundsExercise;
+    @SerializedName("rounds")
+    @Expose
+    private int rounds;
+    @SerializedName("name")
+    @Expose
+    private List<Exercise> name;
+    @SerializedName("list_exercise")
+    @Expose
+    private List<Exercise> exerciseList = new ArrayList<>();
+
+    public int getRestBetweenExercise() {
+        return restBetweenExercise;
+    }
+
+    public void setRestBetweenExercise(int restBetweenExercise) {
+        this.restBetweenExercise = restBetweenExercise;
+    }
+
+    public int getRestRoundsExercise() {
+        return restRoundsExercise;
+    }
+
+    public void setRestRoundsExercise(int restRoundsExercise) {
+        this.restRoundsExercise = restRoundsExercise;
+    }
+
+    public int getRounds() {
+        return rounds;
+    }
+
+    public void setRounds(int rounds) {
+        this.rounds = rounds;
+    }
+
+    public List<Exercise> getName() {
+        return name;
+    }
+
+    public void setName(List<Exercise> name) {
+        this.name = name;
+    }
+
+    public List<Exercise> getExerciseList() {
+        return exerciseList;
+    }
+
+    public void setExerciseList(List<Exercise> exerciseList) {
+        this.exerciseList = exerciseList;
+    }
+}
--- Workhard/app/src/main/java/scott/com/workhard/repository/db/RealmDataService.java
+++ Workhard/app/src/main/java/scott/com/workhard/repository/db/RealmDataService.java
@@ -9,14 +9,14 @@ import scott.com.workhard.repository.db.realm_utils.RealmObservable;
 /**
  * @author pedroscott. scott7462@gmail.com
  * @version 7/18/16.
- * <p>
- * Copyright (C) 2015 The Android Open Source Project
- * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p/>
- *  @see <a href = "http://www.aprenderaprogramar.com" /> http://www.apache.org/licenses/LICENSE-2.0 </a>
+ *          <p>
+ *          Copyright (C) 2015 The Android Open Source Project
+ *          <p/>
+ *          Licensed under the Apache License, Version 2.0 (the "License");
+ *          you may not use this file except in compliance with the License.
+ *          You may obtain a copy of the License at
+ *          <p/>
+ * @see <a href = "http://www.aprenderaprogramar.com" /> http://www.apache.org/licenses/LICENSE-2.0 </a>
  * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
